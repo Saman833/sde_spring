214787c67f7eb4fd80b69793ccb9299c913c4afa
Saman833 <s.ahmadifar2000@gmail.com>
2025-03-03 00:51:42 +0200

    Feat , subscribe by email added , no validation yet

--- src/main/java/sde/homework/demo/controllers/LandingPageController.java
+++ src/main/java/sde/homework/demo/controllers/LandingPageController.java
@@ -2,7 +2,10 @@ package sde.homework.demo.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LandingPageController {
@@ -10,6 +13,11 @@ public class LandingPageController {
     @GetMapping("/")
     public String helloWorld() {
         return "index"; // This will return the index.html page from templates (Thymeleaf) or static (if plain HTML)
+    }
+    @PostMapping("/subscribe")
+    public String subscribe(@RequestParam("email") String email, Model model) {
+        model.addAttribute("message", "Thank you for subscribing with " + email + "!");
+        return "index"; // Redirects back to the landing page with a success message
     }
 }
 
